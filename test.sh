c4ef9f6b7ccaa30c24b017d20fb33e297c9373b7
sututu1 <suwenwang29@gmail.com>
2023-03-03 21:08:07 +0800

    add teat.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 echo "Hello World"
+
+array_name[0]=value0
+array_name[1]=value1
+array_name[2]=value2
+
+#!/bin/bash
+# author:菜鸟教程
+# url:www.runoob.com
+
+my_array=(A B "C" D)
+
+echo "第一个元素为: ${my_array[0]}"
+echo "第二个元素为: ${my_array[1]}"
+echo "第三个元素为: ${my_array[2]}"
+echo "第四个元素为: ${my_array[3]}"
+
+#!/bin/bash
+# author:菜鸟教程
+# url:www.runoob.com
+
+my_array[0]=A
+my_array[1]=B
+my_array[2]=C
+my_array[3]=D
+
+echo "数组的元素为: ${my_array[*]}"
+echo "数组的元素为: ${my_array[@]}"
