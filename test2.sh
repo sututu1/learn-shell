fc9b017847e29d34eea57f6f0c0e0fcd09ce2114
sututu1 <suwenwang29@gmail.com>
2023-03-07 14:54:02 +0800

    add test2.sh

---
+++ test2.sh
@@ -0,0 +1,29 @@
+num1=100
+num2=200
+if test $[num1] -eq $[num2]
+then
+    echo '两个数相等！'
+else
+    echo '两个数不相等！'
+fi
+
+#!/bin/bash
+
+a=5
+b=6
+
+result=$[a*b] # 注意等号两边不能有空格
+echo "result 为： $result"
+result=$[b/a]
+echo "result为: $result"
+result=$[b%a]
+echo "result为: $result"
+
+num1="ru1noob"
+num2="runoob"
+if test $num1 != $num2
+then
+    echo '两个字符串相等!'
+else
+    echo '两个字符串不相等!'
+fi
